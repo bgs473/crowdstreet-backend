e062d06a5a1df8694299928aee21a40437c72fc0
Baldemar Silva <baldemarsilva@Baldemars-MBP.attlocal.net>
2021-03-05 20:17:05 -0600

    Initial commit

---
+++ src/test/java/com/crowdstreet/InterviewAPI/InterviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdstreet.InterviewAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
